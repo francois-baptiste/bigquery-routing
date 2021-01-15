ea5073c6e2e01b6ce80c49da16ccb72953229096
Bogdan BOAMFA <bboamfa.externe@solocal.com>
2021-01-15 15:36:36 +0200

    Update query.

--- find_points_around_query_template.sql
+++ find_points_around_query_template.sql
@@ -1,26 +1,18 @@
-CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.geojson_find_points_around`(geojson STRING, startx FLOAT64, starty FLOAT64, max_cost FLOAT64) 
+CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.find_points_around_from_geojson`(geojson STRING, startx FLOAT64, starty FLOAT64, max_cost FLOAT64) 
 RETURNS STRING LANGUAGE js
 OPTIONS (
   library=["$BUCKET_FILE_PATH"]
 )
 AS """
-  var start = {type: "Feature", geometry: { coordinates:[startx, starty], type: "Point" }};
+  var start = {type: "Feature", geometry: { coordinates: [startx, starty], type: "Point" }};
   var maxCost = max_cost;
   var pathFinder = new geojsonPathFinder(JSON.parse(geojson));
 
   var nodes = pathFinder.findPointsAround(start, maxCost);
   try {
     return JSON.stringify({
-      type: "FeatureCollection",
-      features: [
-        {
-          type: "Feature",
-          geometry: {
-            type: "MultiPoint",
-            coordinates: nodes
-          }
-        }
-      ]
+      type: "MultiPoint",
+      coordinates: nodes
     });
   }
   catch (e) {
@@ -28,7 +20,32 @@ AS """
   }
 """;
 
--- USAGE EXAMPLE
--- select  
---    `data-science-229608.routing_us.geojson_find_points_around`(concat('{"type": "FeatureCollection", "features": [{"type": "Feature","geometry":', string_agg(line, '},{"type":"Feature","geometry":'),"}]}"), -1.028, 46.56, 200 )
--- from `data-science-229608.routing_us.france_network`;
+CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.nearestpoint`(mypoint GEOGRAPHY, mypoints array<GEOGRAPHY>) AS ((
+  WITH EXTRACTED_POINTS AS (
+    SELECT SAFE.ST_GEOGFROMTEXT(CONCAT('POINT(', point, ')')) mypoints
+    FROM unnest(mypoints) geo_object,
+      UNNEST(REGEXP_EXTRACT_ALL(ST_ASTEXT(geo_object), r'[^,\(\)]+')) point WITH OFFSET pos
+    WHERE pos BETWEEN 1 AND ST_NUMPOINTS(geo_object)
+  )
+  SELECT ARRAY_AGG(a.mypoints ORDER BY ST_Distance(a.mypoints, mypoint) LIMIT 1)[ORDINAL(1)] as neighbor_id
+  FROM EXTRACTED_POINTS a
+));
+
+-- 
+CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.find_points_around`(lines array<GEOGRAPHY>, start GEOGRAPHY, max_cost FLOAT64) AS ((
+  WITH SOME_NETWORK AS (
+    SELECT concat('{"type": "FeatureCollection", "features": [{"type": "Feature","geometry":', string_agg(ST_ASGEOJSON(line), '},{"type":"Feature","geometry":'), "}]}") geojson,
+    `$PROJECT_ID.$DATASET.nearestpoint`(start, array_agg(line)) start_nearest,
+    FROM unnest(lines) line
+  ),
+  OUTPUT AS (
+    SELECT `$PROJECT_ID.$DATASET.find_points_around_from_geojson`(geojson, ST_X(start_nearest), ST_Y(start_nearest), max_cost) myresult
+    FROM SOME_NETWORK
+  )
+
+  SELECT * FROM OUTPUT
+));
+
+CREATE OR REPLACE FUNCTION `$PROJECT_ID.$DATASET.get_area_polygon`(lines array<GEOGRAPHY>, start GEOGRAPHY, max_cost FLOAT64) AS ((
+  SELECT ST_CONVEXHULL(`$PROJECT_ID.$DATASET.find_points_around`(lines, start, max_cost))
+));
